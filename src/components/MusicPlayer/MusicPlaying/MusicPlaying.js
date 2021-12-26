82df46d8f4dd906b889c3ee5c17878e75a8f4f44
TuyenQuang <luongtuyenquang77@gmail.com>
2021-12-26 20:09:09 +0700

    Switch Play, Pause Button

--- src/components/MusicPlayer/MusicPlaying/MusicPlaying.js
+++ src/components/MusicPlayer/MusicPlaying/MusicPlaying.js
@@ -1,8 +1,16 @@
 import './MusicPlaying.scss';
 import img1 from '../../../assets/images/song_1.png'
-// import song1 from '../../../assets/mp3/song_1.mp3'
+import song1 from '../../../assets/mp3/song_1.mp3'
+import { useState } from 'react';
 
 function MusicPlaying() {
+
+    const [playing, setPlaying] = useState(true)
+
+    const handlePlayPause = () => {
+        setPlaying(!playing)
+    }
+
     return (
         <div className='music-playing'>
             <p className='music__heading'>Bài hát hiện tại</p>
@@ -12,6 +20,7 @@ function MusicPlaying() {
                 <p className='music-playing__singer'>Lê Bảo Bình</p>
                 <div className='music-playing__range'>
                     <input type='range' />
+                    <audio src={song1} />
                     <div className='music-playing__timer'>
                         <span>4:00</span>
                         <span>2:33</span>
@@ -20,8 +29,11 @@ function MusicPlaying() {
                 <div className='music-playing__control'>
                     <i className='bx bx-shuffle music-playing__control__icon'></i>
                     <i className='bx bx-rewind music-playing__control__icon'></i>
-                    <i className='bx bx-play music-playing__control__icon music-playing__control__icon--color'></i>
-                    {/* <i className='bx bx-pause music-playing__control__icon'></i> */}
+                    <i 
+                        className={`bx bx-${playing ? 'play' : 'pause'} music-playing__control__icon music-playing__control__icon--color`} 
+                        onClick={handlePlayPause}
+                    >
+                    </i>
                     <i className='bx bx-fast-forward music-playing__control__icon'></i>
                     <i className='bx bx-repeat music-playing__control__icon'></i>
                 </div>
