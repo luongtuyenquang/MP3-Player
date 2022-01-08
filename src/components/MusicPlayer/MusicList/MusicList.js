82337c21d386de6dbdedc4adbd65a275ed4c1814
TuyenQuang <luongtuyenquang77@gmail.com>
2022-01-09 00:23:20 +0700

    Empty Search

--- src/components/MusicPlayer/MusicList/MusicList.js
+++ src/components/MusicPlayer/MusicList/MusicList.js
@@ -2,29 +2,36 @@ import { connect } from "react-redux"
 import MusicItem from "../MusicItem/MusicItem"
 import './MusicList.scss'
 import removeVietNameseTones from '../../../utils/removeVietNameseTones.js'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
 function MusicList({music, valueSearch}) {
     
     const [isSort, setIsSort] = useState(true)
+    const [listMusic, setListMusic] = useState([])
     const totalSong = music.length
 
-    const searchSong = (song) => {
-        return song.name.toLowerCase().includes(valueSearch.toString().toLowerCase()) 
-        || song.singer.toLowerCase().includes(valueSearch.toString().toLowerCase())
-        || removeVietNameseTones(song.name.toLowerCase()).includes(valueSearch.toString().toLowerCase())
-        || removeVietNameseTones(song.singer.toLowerCase()).includes(valueSearch.toString().toLowerCase())
-    }
-    
+    const newMusic = music
+
+    useEffect(() => {
+        const searched = newMusic.filter((item) => {
+            return item.name.toLowerCase().includes(valueSearch.toString().toLowerCase()) 
+            || item.singer.toLowerCase().includes(valueSearch.toString().toLowerCase())
+            || removeVietNameseTones(item.name.toLowerCase()).includes(valueSearch.toString().toLowerCase())
+            || removeVietNameseTones(item.singer.toLowerCase()).includes(valueSearch.toString().toLowerCase())
+        })
+        setListMusic(searched)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [valueSearch])
+
     const handleIsSort = () => {
         setIsSort(!isSort)
 
         if(isSort) {
-            music.sort((a, b) => (a.name < b.name) ? 1 : -1)
+            listMusic.sort((a, b) => (a.name < b.name) ? 1 : -1)
      
          } else {
-            music.sort((a, b) => (a.name > b.name) ? 1 : -1)
+            listMusic.sort((a, b) => (a.name > b.name) ? 1 : -1)
          }
     }
 
@@ -44,11 +51,13 @@ function MusicList({music, valueSearch}) {
             </div>
             <div className='music-list__list'>
                 {
-                    music.map((item, index) => {
-                        if(searchSong(item)) {
+                    listMusic.length === 0 
+                    ? 
+                        <span className='search-empty__description'>Không tìm thấy bài hát !</span> 
+                    :
+                        listMusic.map((item, index) => {
                             return <MusicItem item={item} index={index} key={index} />
-                        } else return null
-                    })
+                        })
                 }
             </div>
         </div>
